7d9502de47f1a884a92b84a2c0d75b7088fdbb97
Suneesh Jacob <suneeshjacob@gmail.com>
2021-10-04 01:58:39 +0000

    Made some changes

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var fullrate = principal + interest;
+    var outputString = 'If you deposit '+principal.toString()+',\nat an interest rate of '+rate.toString()+'%.\nYou will receive an amount of '+fullrate.toString()+',\nin the year '+year.toString();
+    document.getElementById("result").innerText=outputString;
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
